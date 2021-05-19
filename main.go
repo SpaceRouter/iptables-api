aeb869461005375d6d27e808fd3460efa03e8592
feloeht <theo.lefevre@edu.esiee.fr>
2021-05-19 21:31:29 +0200

    fixed golint issues

--- main.go
+++ main.go
@@ -13,15 +13,15 @@ import (
 	"strings"
 )
 
+// Constantes
 const (
-	//
 	v6                iptables.Protocol = iota + 1
 	dnatAct           string            = "dnat"
 	snatAct           string            = "snat"
 	logAct            string            = "LOG"
 	trueStr           string            = "true"
 	tcpStr            string            = "tcp"
-	SYNStr            string            = "SYN" // SYNStr const
+	SYNStr            string            = "SYN"
 	defaultFlagsMask  string            = "FIN,SYN,RST,ACK"
 	defaultFlagsMask2 string            = "SYN,RST,ACK,FIN"
 )
