8d680486c8982e27eaaf752254dea14544c747d6
Louis DESPLANCHE <louis.desplanche@edu.esiee.fr>
2021-06-09 17:21:39 +0200

    Rewrite chain_v6

--- src/controllers/chain_v6.go
+++ src/controllers/chain_v6.go
@@ -1,99 +1,152 @@
 package controllers
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/jeremmfr/go-iptables/iptables"
+	"iptables-api/forms"
 	"net/http"
 )
 
 // AddChainV6 PUT /chain_v6/{table}/{name}/
 func AddChainV6(c *gin.Context) {
-	w := c.Writer
-
 	if !checkRole(c) {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, forms.ChainListResponse{
+			Ok:      false,
+			Message: "",
+		})
 		return
 	}
 
 	ipt, err := iptables.NewWithProtocol(v6)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
 		return
 	}
-	respErr = ipt.NewChain(c.Param("table"), c.Param("name"))
-	if respErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, respErr)
+	err = ipt.NewChain(c.Param("table"), c.Param("name"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
+		return
 	}
+
+	c.JSON(http.StatusOK, forms.ChainListResponse{
+		Ok:      true,
+		Message: "",
+	})
 }
 
 // DelChainV6 DELETE /chain_v6/{table}/{name}/
 func DelChainV6(c *gin.Context) {
-	w := c.Writer
-
 	if !checkRole(c) {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, forms.ChainListResponse{
+			Ok:      false,
+			Message: "",
+		})
 		return
 	}
-
 	ipt, err := iptables.NewWithProtocol(v6)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
+		return
+	}
+	// Clear chain before delete
+	err = ipt.ClearChain(c.Param("table"), c.Param("name"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
+		return
+	}
+	// Delete chain
+	err = ipt.DeleteChain(c.Param("table"), c.Param("name"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
 		return
 	}
 
-	// Clear chain before delete
-	respErr = ipt.ClearChain(c.Param("table"), c.Param("name"))
-	if respErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, respErr)
-	}
-	// Delete chain
-	respErr = ipt.DeleteChain(c.Param("table"), c.Param("name"))
-	if respErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, respErr)
-	}
+	c.JSON(http.StatusOK, forms.ChainListResponse{
+		Ok:      true,
+		Message: "",
+	})
 }
 
 // ListChainV6 GET /chain_v6/{table}/{name}/
 func ListChainV6(c *gin.Context) {
-	w := c.Writer
-
 	if !checkRole(c) {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, forms.ChainListResponse{
+			Ok:      false,
+			Message: "",
+		})
 		return
 	}
 
 	ipt, err := iptables.NewWithProtocol(v6)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
 		return
 	}
-	respStr, respErr := ipt.List(c.Param("table"), c.Param("name"))
-	if respErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, respErr)
+	respStr, err := ipt.List(c.Param("table"), c.Param("name"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
+		return
 	}
-	for i := 0; i < len(respStr); i++ {
-		fmt.Fprintln(w, respStr[i])
-	}
+
+	chains := stringToChains(respStr)
+
+	c.JSON(http.StatusOK, forms.ChainListResponse{
+		Ok:      true,
+		Message: "",
+		Chains:  chains,
+	})
 }
 
 // RenameChainV6 PUT /mvchain_v6/{table}/{oldname}/{newname}/
 func RenameChainV6(c *gin.Context) {
-	w := c.Writer
-
 	if !checkRole(c) {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, forms.ChainListResponse{
+			Ok:      false,
+			Message: "",
+		})
 		return
 	}
 
 	ipt, err := iptables.NewWithProtocol(v6)
 	if err != nil {
-		http.Error(w, err.Error(), 500)
+		c.AbortWithStatusJSON(http.StatusInternalServerError, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
 		return
 	}
-	respErr = ipt.RenameChain(c.Param("table"), c.Param("oldname"), c.Param("newname"))
-	if respErr != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprintln(w, respErr)
+	err = ipt.RenameChain(c.Param("table"), c.Param("oldname"), c.Param("newname"))
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusBadRequest, forms.ChainListResponse{
+			Ok:      false,
+			Message: err.Error(),
+		})
+		return
 	}
+
+	c.JSON(http.StatusOK, forms.ChainListResponse{
+		Ok:      true,
+		Message: "",
+	})
 }
